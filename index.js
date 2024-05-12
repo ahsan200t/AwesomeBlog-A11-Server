518b9868dad9891319d7b7effa2f6a0610d66203
ahsan200t <ahsanpekua200@gmail.com>
2024-05-13 00:09:53 +0600

    Wishlist's data get Done

--- index.js
+++ index.js
@@ -29,64 +29,75 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     const blogCollection = client.db('blogDB').collection('blog');
-    const commentCollection=client.db('blogDB').collection('comment')
-    const wishCollection=client.db('blogDB').collection('wihsList')
+    const commentCollection = client.db('blogDB').collection('comment')
+    const wishCollection = client.db('blogDB').collection('wishlist')
 
     // Post blog
     app.post('/blogs', async (req, res) => {
-        const newBlog = req.body;
-        const result = await blogCollection.insertOne(newBlog);
-        res.send(result);
-      });
+      const newBlog = req.body;
+      const result = await blogCollection.insertOne(newBlog);
+      res.send(result);
+    });
 
-      // Post Comment
-      app.post('/comments', async(req,res)=>{
-        const newComment=req.body;
-        const result=await commentCollection.insertOne(newComment);
-        res.send(result)
-      })
-
-      // Post WishList
-      app.post('/wish', async (req,res)=>{
-        const newWishList=req.body;
-        const result=await wishCollection.insertOne(newWishList);
-        res.send(result)
-      })
-
-      app.put('/blog/:id', async(req,res)=>{
-        const id=req.params.id;
-        const filter={_id: new ObjectId(id)};
-        const options = { upsert: true };
-        const updateBlog=req.body;
-        const blog={
-          $set:{
-            title: updateBlog.title,
-            longdescription: updateBlog.longdescription,
-            photo: updateBlog.photo,
-            email: updateBlog.email,
-            description: updateBlog.description,
-          }
-        }
-        const result=await blogCollection.updateOne(filter,blog,options);
-        res.send(result)
-      })
-
-      app.get('/comments', async(req,res)=>{
-        const cursor=commentCollection.find();
-        const result=await cursor.toArray();
-        res.send(result)
-      })
-
-    app.get('/blogs', async (req, res) => {
-        const cursor = blogCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
+    // Post Comment
+    app.post('/comments', async (req, res) => {
+      const newComment = req.body;
+      const result = await commentCollection.insertOne(newComment);
+      res.send(result)
     })
 
-    app.get('/blogs/:id', async(req,res)=>{
-      const id =req.params.id;
-      const query={_id: new ObjectId(id)};
-      const result =await blogCollection.findOne(query);
+    // Post WishList
+    app.post('/wish', async (req, res) => {
+      const newWishList = req.body;
+      const result = await wishCollection.insertOne(newWishList);
+      res.send(result)
+    })
+
+    // Wish List Get
+    app.get('/blogs/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await wishCollection.find(query).toArray();
+      res.send(result)
+    })
+    // Update Blog's put
+    app.put('/blog/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateBlog = req.body;
+      const blog = {
+        $set: {
+          title: updateBlog.title,
+          longdescription: updateBlog.longdescription,
+          photo: updateBlog.photo,
+          email: updateBlog.email,
+          description: updateBlog.description,
+        }
+      }
+      const result = await blogCollection.updateOne(filter, blog, options);
+      res.send(result)
+    })
+
+    // Comments section get
+    app.get('/comments', async (req, res) => {
+      const cursor = commentCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    // All Blog Page's Get
+    app.get('/blogs', async (req, res) => {
+      const cursor = blogCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    // Blog Details Get
+    app.get('/blogs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogCollection.findOne(query);
       res.send(result);
     })
 
@@ -103,9 +114,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send("server is running")
+  res.send("server is running")
 });
 
 app.listen(port, () => {
-    console.log(`service running on port: ${port}`)
+  console.log(`service running on port: ${port}`)
 })
