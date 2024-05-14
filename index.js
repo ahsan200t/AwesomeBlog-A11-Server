98cc78a8c769d919bf8b6aa5f039e9d645c7fc28
ahsan200t <ahsanpekua200@gmail.com>
2024-05-14 17:39:37 +0600

    server side Data Updated

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const jwt=require("jsonwebtoken");
-const cookieParser=require('cookie-parser')
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const corsOptions={
-  origin:[
+const corsOptions = {
+  origin: [
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'https://assignment-11-de1f3.web.app',
+    'https://assignment-11-de1f3.firebaseapp.com',
+    'https://awesome-blog-steel.vercel.app'
 
   ],
- credentials:true,
- optionSuccessStatus:200,
+  credentials: true,
+  optionSuccessStatus: 200,
 }
 
 // middleWere
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser())
+
+// verify jwt middleware
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) return res.status(401).send({ message: "unauthorized access" })
+  if (token) {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        console.log(err)
+        return res.status(401).send({ message: "unauthorized access" })
+      }
+      console.log(decoded)
+      req.user = decoded
+      next()
+    })
+  }
+}
+
 
 
 
@@ -42,27 +65,37 @@ async function run() {
     const blogCollection = client.db('blogDB').collection('blog');
     const commentCollection = client.db('blogDB').collection('comment')
     const wishCollection = client.db('blogDB').collection('wishlist')
-     
+
     // jwt generate
-     app.post('/jwt', async(req,res)=>{
-      const user=req.body;
-      const token=jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "7d"
       })
       // res.send(token)
       res.cookie('token', token, {
-        httpOnly:true,
-        secure: process.env.NODE_ENV==='production',
-        sameSite:process.env.NODE_ENV === 'production' ? 'none': 'strict'
-      }).send({success: true})
-     })
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+      }).send({ success: true })
+    })
+
+    //  Clear token
+    app.get('/logout', (req, res) => {
+      res.clearCookie('token', {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        maxAge: 0
+      }).send({ success: true })
+    })
 
 
     // Post blog
     app.post('/blogs', async (req, res) => {
       const newBlog = req.body;
       const result = await blogCollection.insertOne(newBlog);
-      res.send(result);
+      res.send(result,allResult);
     });
 
     // Post Comment
@@ -89,7 +122,7 @@ async function run() {
     // Wish List Delete
     app.delete('/wish/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id) };
+      const query = { _id: new ObjectId(id) };
       const result = await wishCollection.deleteOne(query)
       res.send(result)
     })
@@ -121,13 +154,17 @@ async function run() {
 
     // All Blog Page's Get
     app.get('/blogs', async (req, res) => {
+      // const filter=req.query.filter;
+      // let query={}
+      // if(filter) query={category:filter}
+      // const allResult=await blogCollection.find(query)
       const cursor = blogCollection.find();
       const result = await cursor.toArray();
       res.send(result)
     })
 
     // Blog Details Get
-    app.get('/blogs/:id', async (req, res) => {
+    app.get('/single-blogs/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await blogCollection.findOne(query);
@@ -135,7 +172,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -151,5 +188,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`service running on port: ${port}`)
+  console.log(`server running on port: ${port}`)
 })
